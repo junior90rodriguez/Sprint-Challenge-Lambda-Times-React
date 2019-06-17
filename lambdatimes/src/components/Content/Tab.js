5ee496b3893bc44282f92a35fb664d1670e4fbb6
Junior <junior.arodriguez@icloud.com>
2019-06-16 21:06:15 -0500

    added proptypes to Tab

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import PropTypes from 'prop-types';
+
 const Tab = props => {
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
@@ -17,5 +19,9 @@ const Tab = props => {
 };
 
 // Make sure you include PropTypes on your props.
+
+Tab.propTypes = {
+  tab: PropTypes.string
+}
 
 export default Tab;
