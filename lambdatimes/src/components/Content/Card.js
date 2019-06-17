abb06ce9b33d52ca99d2b08689eafc1421175995
Junior <junior.arodriguez@icloud.com>
2019-06-16 20:33:08 -0500

    Added proptypes

--- lambdatimes/src/components/Content/Card.js
+++ lambdatimes/src/components/Content/Card.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import PropTypes from 'prop-types'
+
 const Card = props => {
   return (
     <div className="card">
@@ -15,5 +17,13 @@ const Card = props => {
 };
 
 // Make sure to include PropTypes.
+Card.propTypes = {
+  card: PropTypes.shape({
+    headline: PropTypes.string,
+    tab: PropTypes.string,
+    img: PropTypes.string,
+    author: PropTypes.string
+  })
+}
 
 export default Card;
